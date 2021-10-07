bea1c99ca6c1e552548e57f4cf73278109fef8e1
Irvin Torres <irvinrexx@gmail.com>
2021-10-07 17:38:56 -0600

    Init ProfessionalController and related views

---
+++ src/main/java/sv/edu/udb/www/jobboard/controllers/ProfessionalController.java
@@ -0,0 +1,41 @@
+package sv.edu.udb.www.jobboard.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/p")
+public class ProfessionalController {
+    @GetMapping(path = "/profile")
+    public String getProfile(Model model){
+        return "professional/profile";
+    }
+
+    @GetMapping(path = "/resume")
+    public String getResume(Model model){
+        return "professional/resume";
+    }
+
+    @GetMapping(path = "/job-offer")
+    public String getJobOffer(Model model){
+        return "professional/job-offer";
+    }
+
+    @GetMapping(path = "/applications")
+    public String getApplications(Model model){
+        return "professional/applications";
+    }
+
+    @GetMapping(path = "/display-job-offer")
+    public String displayJobOffer(Model model){
+        return "professional/display-job-offer";
+    }
+
+    @GetMapping(path = "/apply")
+    public String displayApply(Model model){
+        return "professional/apply";
+    }
+
+}
